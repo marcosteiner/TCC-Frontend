efb750075a7c71677c4945c4bfff25c98c9bb974
marcosteiner <marco.steiner@windowslive.com>
2019-05-09 01:08:33 +0200

    Added total coffee count to header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { Grid, Table, Button, Icon } from 'semantic-ui-react';
+import { Grid, Table, Button, Icon, Header } from 'semantic-ui-react';
 import {getUserData} from './converter';
 
 const TableExampleCelledStriped = (props) => (
   <Grid celled='internally'>
+    <GridHead/>
+    <TotalCount users={props.users}/>
     <GridBody users={props.users}/>
   </Grid>
 )
+
+const GridHead = (props) => {
+  return (
+    <Grid.Row>
+      <Grid.Column>
+        <Header as='h1'>The Coffee Couter (TCC)</Header>
+      </Grid.Column>
+    </Grid.Row>
+  );
+}
+
+const TotalCount = (props) => {
+  let totalCount = coffeeSum(props.users);
+  
+  return (
+    <Grid.Row>
+      <Grid.Column>
+        <Header as='h3'>Total: {totalCount} Coffees</Header>
+      </Grid.Column>
+    </Grid.Row>
+  );
+}
+
+const coffeeSum = (users) => {
+  let sum = 0;
+  for (let user of users){
+    for(let coffee of user.consumptionData){
+      sum += coffee.consumed;
+    }
+  }
+  return sum;
+}
 
 const GridBody = (props) => {
   return props.users.map(u => {
