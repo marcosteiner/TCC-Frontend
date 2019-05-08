504386095f0680b6be714eec839fbcaf7f42f032
marcosteiner <marco.steiner@windowslive.com>
2019-05-08 17:40:39 +0200

    Created styled table with users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Icon, Table } from 'semantic-ui-react'
 import './App.css';
+import { getUsers } from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TableExampleCelledStriped = (props) => (
+  <Table celled striped>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell >The Coffee Counter (TCC)</Table.HeaderCell>
+      </Table.Row>
+      <Table.Row>
+        <Table.HeaderCell >Total: 7 Coffees</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      <TableBody users={props.users}/>
+    </Table.Body>
+  </Table>
+)
+
+function TableBody(props) {
+  return props.users.map(u => {
+    const rowKey = `${u.name}`;
+    return (
+      <Table.Row key={rowKey}>
+        <Table.Cell >{u.name}</Table.Cell>
+      </Table.Row>
+    )
+  });
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      users: getUsers(),
+    }
+  }
+
+  render() {
+    return (
+      <TableExampleCelledStriped
+        users={this.state.users}
+      />
+    );
+  }
 }
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,5 @@
+const backend = "http://localhost:3000";
+
+export function getUsers(){
+    return [{"name": "Marco"},{"name": "Nicola"},{"name": "Pascal"}];
+}
