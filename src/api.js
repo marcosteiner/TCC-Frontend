49a55343a1c612f5cc493e201dfe8435f5f18306
marcosteiner <marco.steiner@windowslive.com>
2019-05-08 21:31:16 +0200

    Returned some placeholder values in api calls

--- src/api.js
+++ src/api.js
@@ -1,5 +1,11 @@
-const backend = "http://localhost:3000";
-
 export function getUsers(){
     return [{"name": "Marco"},{"name": "Nicola"},{"name": "Pascal"}];
+}
+
+export function getCoffees(){
+    return [{"name": "Milchkaffee"},{"name": "Espresso"},{"name": "Schale"}];
+}
+
+export function getConsumptionData(){
+    return [{"coffee_name":"Milchkaffee","person_name":"Marco","coffee_count":27},{"coffee_name":"Espresso","person_name":"Marco","coffee_count":2},{"coffee_name":"Milchkaffee","person_name":"Pascal","coffee_count":27}];
 }
